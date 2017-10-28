4fa88be02900225b66e12e206203a8b6293433a7
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2017-10-28 23:18:54 +0000

    todo: move clear dir to 'cleanup' file

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-
-import os, glob
+import os
 
 def main():
-	dir_init()
+	clearDir = input("Would you like to clear the current directories?(1/0)\n")
+	dir_init(clearDir)
 
-####################
-# Make Directories #
-####################
-def dir_init():
+########################
+#   Make Directories   #
+########################
+def dir_init(clearDir=0):
+	#create images folder
 	if not os.path.exists("images"):
 		os.makedirs("images")
-	if not os.path.exists("video"):
-		os.makedirs("video")
-	
+	#create videos folder
+	if not os.path.exists("videos"):
+		os.makedirs("videos")
+	#if clear flag is true, clear folder contents
+	if(clearDir == 1):
+		for photo in os.listdir("images"):
+			file_path = os.path.join("images",photo)
+			try:
+				if os.path.isfile(file_path):
+					os.unlink(file_path)
+			except Exception as e:
+				print(e)
+		for video in os.listdir("videos"):
+			file_path = os.path.join("videos",video)
+			try:
+				if os.path.isfile(file_path):
+					os.unlink(file_path)
+			except Exception as e:
+				print(e)
 
 
 if __name__ == "__main__":
