7f5d050df6b13fe7b27843bee37049bfcc7ca4d6
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2017-10-29 21:39:43 +0000

    cleanup from main to standalone

--- cleanup.py
+++ cleanup.py
@@ -1,7 +1,28 @@
 import os
 
 def main():
-	
+	images = input("Clear out images directory?(1/0)\n")
+	videos = input("Clear out videos directory?(1/0)\n")
+
+	if(images == 1):
+		print("Cleaning images directory...")
+		if not os.path.exists("images"):
+			print("Images directory doesn't exist.")
+		else:
+			for file in os.listdir("images"):
+				path = os.path.join("images",file)
+				os.unlink(path)
+		print("Finished cleaning.")
+	if(videos == 1):
+		print("Cleaning videos directory...")
+		if not os.path.exists("videos"):
+			print("Videos directory does not exist.")
+		else:
+			for file in os.listdir("videos"):
+				path = os.path.join("videos",file)
+				os.unlink(path)
+		print("Finished cleaning.")
+	print("Done.")
 
 if __name__ == "__main__":
 	main()
--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import os
 
 def main():
-	clearDir = input("Would you like to clear the current directories?(1/0)\n")
-	dir_init(clearDir)
+	print("Initializing Directories...")
+	dir_init()
 
 ########################
 #   Make Directories   #
 ########################
 def dir_init(clearDir=0):
 	#create images folder
+	print("Creating images directory...")
 	if not os.path.exists("images"):
 		os.makedirs("images")
+		if os.path.exists("images"):
+			print("Images directory created.")
+		elif not os.path.exists("images"):
+			print("Failed to create images directory.")
+	elif os.path.exists("images"):
+		print("Images directory already exists.")
 	#create videos folder
+	print("Creating videos directory...")
 	if not os.path.exists("videos"):
 		os.makedirs("videos")
-
+		if os.path.exists("videos"):
+			print("Videos directory created.")
+		elif not os.path.exists("vidoes"):
+			print("Failed to create images directory.")
+	elif os.path.exists("videos"):
+		print("Videos directory already exists.")
 
 if __name__ == "__main__":
 	main()
