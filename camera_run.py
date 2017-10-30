9d83313a56f5ba05066567138251b65aca281377
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2017-10-30 18:13:07 +0000

    Photos working

--- camera_run.py
+++ camera_run.py
@@ -1,49 +1,49 @@
 from time import sleep
-import picamera import PiCamera
+from picamera import PiCamera
 
-camera = Picamera()
-
+camera = PiCamera()
 ####################################
 # Main function, pilots code
 ####################################
 def main():
-	while(TRUE):
+	take_picture()
 
 
 ####################################
 # Takes video for a specified time
 ####################################
-def take_video(time):
-#Sensor mode 1: 1920x1080, 1/10<=fps<=30
-camera.sensor_mode(1)
+def take_picture():
+	#Sensor mode 1: 1920x1080, 1/10<=fps<=30
+	camera.sensor_mode = 1
 
-#warmup camera
-camera.start_preview()
-sleep(2)
+	#warmup camera
+	camera.start_preview()
+	sleep(2)
 
-#take photo
-camera.capture("%t.jpg")
+	#take photo
+	camera.capture("test.jpeg",quality = 100)
 
-#prepare for mode change
-camera.stop_preview()
+	#prepare for mode change
+	camera.stop_preview()
 
 
 ####################################
 # Takes high resolution photo
 ####################################
-def take_picture():
-#Sensor mode 2: 3280x2464, 1/10<=fps<=15
-camera.sensor_mode(2)
-#warm up camera
-camera.start_preview()
-sleep(2)
+def take_video(time):
+	#Sensor mode 2: 3280x2464, 1/10<=fps<=15
+	camera.sensor_mode = 2
+	#warm up camera
+	camera.start_preview()
+	sleep(2)
 
-camera.start_recording(quality = 10)
-sleep(time)
-camera.stop_recording()
+	#take video
+	camera.start_recording("test.h264",quality = 10)
+	sleep(time)
+	camera.stop_recording()
 
-#prepare for mode change
-camera.stop_preview()
+	#prepare for mode change
+	camera.stop_preview()
 
 
 if __name__ == "__main__":
