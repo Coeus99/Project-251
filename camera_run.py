4d0f5326dcb950f446499784537b0f357c9f40cc
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2017-11-01 19:32:13 +0000

    Created class for high altitude ballooning photos

--- camera_run.py
+++ camera_run.py
@@ -3,55 +3,30 @@ from picamera import PiCamera
 from datetime import datetime
 
 camera = PiCamera()
-####################################
-# Main function, pilots code
-####################################
-def main():
-	videoLength = 60
-	take_picture()
-	take_video(videoLength)
 
+class run_camera:
+	def take_picture(self):
+		#Sensor mode 2: 3280x2464, 1/10<=fps<=15
+		camera.sensor_mode = 2
+		#warmup camera
+		camera.start_preview()
+		sleep(2)
+		#take photo
+		filename = datetime.now().strftime("%Y-%m-%d--%H%M.jpeg")
+		camera.capture(filename,quality=100)
+		#prepare for mode change
+		camera.stop_preview()
 
-####################################
-# Takes picture at high res
-####################################
-def take_picture():
-	#Sensor mode 2: 3280x2464, 1/10<=fps<=15
-	camera.sensor_mode = 2
-
-	#warmup camera
-	camera.start_preview()
-	sleep(2)
-
-	filename = datetime.now().strftime("%Y-%m-%d--%H%M.jpeg")
-	#take photo
-	camera.capture(filename,quality=100)
-
-	#prepare for mode change
-	camera.stop_preview()
-
-
-####################################
-# Takes video given a length
-####################################
-def take_video(length):
-	#Sensor mode 2: 1920x1080, 1/10<=fps<=30
-	camera.sensor_mode = 1
-	#camera.resolution = (1280,720) #this works for some reason...
-
-	#warm up camera
-	camera.start_preview()
-	sleep(2)
-
-	filename = datetime.now().strftime("%Y-%m-%d--%H%M.h264")
-	#take video
-	camera.start_recording(filename)
-	camera.wait_recording(length)
-	camera.stop_recording()
-
-	#prepare for mode change
-	camera.stop_preview()
-
-
-if __name__ == "__main__":
-	main()
+	def take_video(self,videoLength):
+		#Sensor mode 2: 1920x1080, 1/10<=fps<=30
+		camera.sensor_mode = 1
+		#warm up camera
+		camera.start_preview()
+		sleep(2)
+		#take video
+		filename = datetime.now().strftime("%Y-%m-%d--%H%M.h264")
+		camera.start_recording(filename)
+		camera.wait_recording(videoLength)
+		camera.stop_recording()
+		#prepare for mode change
+		camera.stop_preview()
