876064b3c6b75e1f658da028af11d7b970b41af4
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2017-11-06 17:07:00 +0000

    Combined files into main, not working after a certain point

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
-from camera_run import run_camera
+import picamera
 from time import sleep
+from datetime import datetime
 
-camera = run_camera()
 
 def main():
 	#Initialize directories required
 	dir_init()
 	print("Running...")
-	for x in range(12):
-		camera.take_picture()
-		camera.take_video(300)
+	for x in range(120):
+		print(str(x+1)+" of "+str(120))
+		take_image()
+		sleep(1)
+		take_video(300)
+		sleep(1)
+	print("Done.")
 
 ########################
 #   Make Directories   #
@@ -37,7 +41,40 @@ def dir_init():
 			print("Failed to create images directory.")
 	elif os.path.exists("videos"):
 		print("Videos directory already exists.")
-	print("Done.")
+	print("Done.\n")
+
+def take_image():
+	with picamera.PiCamera() as camera:
+		#Sensor mode 2: 3280x2464, 1/10<=fps<=15
+		print("Changing sensor mode...")
+		camera.sensor_mode = 2
+		print("Sensor mode is now "+str(camera.sensor_mode))
+		#warmup camera
+		print("Beginning preview...")
+		camera.start_preview()
+		sleep(2)
+		print("Taking image...")
+		filename = "images/"+datetime.now().strftime("%Y-%m-%d--%H%M.jpeg")
+		camera.capture(filename,quality=100)
+		camera.stop_preview()
+
+
+def take_video(videoLength):
+	with picamera.PiCamera() as camera:
+		#Sensor mode 1: 1920.1080, 1/10<=fps<=30
+		print("Changing sensor mode...")
+		camera.sensor_mode = 1
+		print("Sensor mode is now "+str(camera.sensor_mode))
+		print("Beginning preview...")
+		camera.start_preview()
+		sleep(2)
+		print("Taking video for "+str(videoLength)+" seconds...")
+		filename = "videos/"+datetime.now().strftime("%Y-%m-%d--%H%M.h264")
+		camera.start_recording(filename,quality=1)
+		camera.wait_recording(videoLength)
+		camera.stop_recording()
+		camera.stop_preview()
+
 
 if __name__ == "__main__":
 	main()
