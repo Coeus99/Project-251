aa5ea83a7b85eba267fe753fa9f07880e055973a
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2017-12-22 11:48:50 -0700

    rid of old in with new

--- driver.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-if [[ -e /home/pi/Documents/Project-Nyx/images/image-0.jpeg ]] ; then
-	echo "Nyx has already ran, please empty directories."
-else
-	while true ; do bash /home/pi/Documents/Project-Nyx/nyx.sh ; done
-fi
