c08691d6a7555efac5f7c25333404fe46945ab81
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2018-02-19 15:30:33 -0700

    Fixed issue where nyx wouldn't take multiple pictures. (had to change -0 to -o... I')

--- nyx.sh
+++ nyx.sh
@@ -7,20 +7,14 @@ image_filename=/home/pi/Project-Nyx/images/image
 while [[ -e $image_filename-$i.jpeg ]] ; do
 	let i++
 done
-image_filename=$image_filename-$i.jpeg
+image_filename=$image_filename-$i
 #take that image
 #mode 2: 3280x2464, Full FOV
-#I'm using this mode, no argument, I want good pictures
-raspistill -t 2000 -md 2 -q 100 -vf -hf -o $image_filename -n
-raspistill -t 2000 -md 2 -q 100 -vf -hf -0 $image_filename-0 -n
-raspistill -t 2000 -md 2 -q 100 -vf -hf -0 $image_filename-1 -n
-raspistill -t 2000 -md 2 -q 100 -vf -hf -0 $image_filename-2 -n
-raspistill -t 2000 -md 2 -q 100 -vf -hf -0 $image_filename-3 -n
-#-md 2, switch to mode 2
-#-t 2000, take photo after 2 seconds
-#-q 100, set image quality to 100 (0-100)
-#-o $image_filename, output to file
-#-n ,no camera preview
+raspistill -t 2000 -md 2 -q 100 -vf -hf -o $image_filename.jpeg -n
+raspistill -t 2000 -md 2 -q 100 -vf -hf -o $image_filename-0.jpeg -n
+raspistill -t 2000 -md 2 -q 100 -vf -hf -o $image_filename-1.jpeg -n
+raspistill -t 2000 -md 2 -q 100 -vf -hf -o $image_filename-2.jpeg -n
+raspistill -t 2000 -md 2 -q 100 -vf -hf -o $image_filename-3.jpeg -n
 
 #write to disk, we don't want to lose anything!
 sync
@@ -41,7 +35,7 @@ video_filename=$video_filename-$v.h264
 #mode 5: 1640x922 @ 0.1-40fps, full FOV
 #mode 6: 1280x720 @ 40-90fps, partial FOV
 #mode 7: 640x480 # 40-90fps, partial FOV
-raspivid -md 2 -t 300000 -vf -hf -o $video_filename -n
+raspivid -md 2 -t 5000 -vf -hf -o $video_filename -n
 #-md 1, set to mode 1
 #-t 300000, take video for 5 minutes
 #-o $video_filename, output video to file
