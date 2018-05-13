aaee2582f2e261871c09f22cbdae1af9879dbe47
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2018-05-12 18:56:00 -0600

    filename is getting found by date command

--- nyx.sh
+++ nyx.sh
@@ -4,6 +4,14 @@
 #save with datetime
 #go to milli-second
 
+fn=$(date -u +%Y%m%d%H%M%S)
+echo "Took photo $fn"
+sleep 2
+
 #take video of n length
 #save with datetime
 #got to milli-second
+
+fn=$(date -u +%Y%m%d%H%M%S)
+echo "Took video $fn"
+sleep 2
