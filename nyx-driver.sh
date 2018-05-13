a0c63e58902c146cde8742f94b8839631552dde5
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2018-05-13 13:55:44 -0600

    driver update

--- nyx-driver.sh
+++ nyx-driver.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-while true ; do bash /home/pi/Project-Nyx/nyx.sh >> /home/pi/Project-Nyx/error-logs/driver-err.log 2>&1 ; done
+nyx="/home/pi/Project-Nyx/nyx.sh"
+
+while true
+do
+	bash $nyx >> /home/pi/Project-Nyx/logs/nyx.log 2>&1
+done
