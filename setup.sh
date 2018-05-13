3a6198bf23c4b60db7a3d9cc40f8fede4795db19
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2018-05-13 14:32:26 -0600

    added system script and updated setup for new log directory

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-LOGDIR="/home/pi/Project-Nyx/error-logs"
+LOGDIR="/home/pi/Project-Nyx/logs"
 IMGDIR="/home/pi/Project-Nyx/images"
 VIDDIR="/home/pi/Project-Nyx/videos"
 
 if ! [ -d $LOGDIR ]
 then
-	mkdir /home/pi/Project-Nyx/error-logs
+	mkdir /home/pi/Project-Nyx/logs
 fi
 
 if ! [ -d $IMGDIR ]
---
+++ system.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+log="/home/pi/Project-Nyx/logs/system.log"
+
+
+while true
+do
+	#get system time
+	echo -e "time=$(date +%Y%m%d%H%M%S)" >> $log 2>&1
+
+	#get core temperature
+	echo -e "$(vcgencmd measure_temp)" >> $log 2>&1
+
+	#get voltages
+	for id in core sdram_c sdram_i sdram_p
+	do
+		echo -e "$id:\t$(vcgencmd measure_volts $id)" >> $log 2>&1
+	done
+	echo -e "\n" >> $log 2>&1
+
+	sleep 10
+done
