db981ae293f1073b00d2b27429ee57c4fbef9387
Coeus99 <Logan.Grosz@mines.sdsmt.edu>
2018-06-26 17:28:50 -0600

    System log is now in csv format

--- system.sh
+++ system.sh
@@ -5,18 +5,23 @@ log="/home/pi/Project-Nyx/logs/system.log"
 
 while true
 do
-	#get system time
-	echo -e "time=$(date -u +%Y%m%d%H%M%S)" >> $log 2>&1
+	#get date
+	echo -n "$(date -u +%Y%m%d)," >> $log 2>&1
+
+	#get time
+	echo -n "$(date -u +%H%M%S)," >> $log 2>&1
 
 	#get core temperature
-	echo -e "$(vcgencmd measure_temp)" >> $log 2>&1
+	tempvar="$(vcgencmd measure_temp)"
+	echo -n ${tempvar:5}, >> $log 2>&1
 
 	#get voltages
 	for id in core sdram_c sdram_i sdram_p
 	do
-		echo -e "$id:\t$(vcgencmd measure_volts $id)" >> $log 2>&1
+		tempvar="$(vcgencmd measure_volts $id)"
+		echo -n ${tempvar:5},  >> $log 2>&1
 	done
-	echo -e "\n" >> $log 2>&1
+	echo -n -e "\r\n" >> $log 2>&1
 
 	sleep 10
 done
